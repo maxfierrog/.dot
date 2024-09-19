64c82c98a6c3b9728d1f6669079ee0350fe9c244
Max Fierro <maxfierro@berkeley.edu>
2024-09-19 00:32:59 -0700

    Removed spelling checks

--- content/.config/nvim/lua/maxfierro/init.lua
+++ content/.config/nvim/lua/maxfierro/init.lua
@@ -7,9 +7,6 @@ vim.cmd('set tabstop=4')
 vim.cmd('set softtabstop=0')
 vim.cmd('set shiftwidth=0')
 
-vim.opt.spelllang = 'en_us'
-vim.opt.spell = true
-
 vim.diagnostic.config({
 	virtual_text = false,
 	signs = true,
--- content/.config/nvim/lua/maxfierro/remap.lua
+++ content/.config/nvim/lua/maxfierro/remap.lua
@@ -1,7 +1,3 @@
-local function spell_correct()
-	vim.cmd('normal! <C-g>u<Esc>[s1z=`]a<C-g>u')
-end
-
 vim.g.mapleader = " "
 vim.keymap.set("n", "<bs>", '<c-^>zz', { silent = true, noremap = true })
 vim.keymap.set("n", "<Tab>", "<C-w>w")
@@ -9,5 +5,3 @@ vim.keymap.set("n", "<Space>s", ":w<CR>")
 vim.keymap.set("n", "<Space>q", ":q<CR>")
 vim.keymap.set("n", "<Space>Q", ":wa<CR>:qa<CR>")
 vim.keymap.set("n", "<Space>S", ":wa<CR>")
-
-vim.keymap.set("i", "<C-s>", "<Cmd>lua spell_correct()<CR>", { noremap = true, silent = true })
